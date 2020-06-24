ae357b7564a3d41cd53077dd4954887b28193750
Enrico <enricosellanora96@gmail.com>
2020-06-24 16:51:05 +0200

    Completo con bonus

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/*chiedete all'utente un cognome e aggiungetelo alla lista, poi ordinate, utilizzate una funzionalità di js per l'ordinamento.*/
+
+var nomi = ["Giovanni","Maurizio", "Chiara", "Giulia", "Giulio"];
+nomi = nomi.sort();
+console.log(nomi);
---
+++ js/scriptbonus.js
@@ -0,0 +1,34 @@
+/*chiedete all'utente un cognome e aggiungetelo alla lista, poi ordinate, utilizzate una funzionalità di js per l'ordinamento.*/
+
+var nomi = ["Giovanni","Maurizio", "Chiara", "Giulia", "Giulio"];
+var nuovoNome="";
+
+while (nuovoNome == "") {
+  var nuovoNome = prompt("Inserisci un nome");
+}
+nomi.push(nuovoNome);
+console.log(nomi);
+console.log(nomi.length);
+var j=0;
+while (j != nomi.length) {
+  for (var i = 0; i < nomi.length - 1 ; i++) {
+    var scambio = nomi[i].localeCompare(nomi[i+1]);
+    console.log(nomi[i]);
+    console.log(nomi[i+1]);
+    console.log(scambio);
+    if (scambio != -1) {
+      var primoScambio1 = nomi[i];
+      var primoScambio2 = nomi[i+1];
+      var secondoScambio1 = nomi[i+1];
+      var secondoScambio2 = nomi[i];
+      primoScambio1 = primoScambio2;
+      secondoScambio1 = secondoScambio2;
+      nomi[i] = primoScambio1
+      nomi[i+1] = secondoScambio1
+    }
+  }
+  j++;
+}
+
+
+console.log(nomi);
